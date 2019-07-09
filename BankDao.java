d72150074e9984d2e64dda7bd33a298c23ad03fa
Aviral-Agrawal123 <39653678+Aviral-Agrawal123@users.noreply.github.com>
2019-07-09 16:37:46 +0530

    Add files via upload

---
+++ BankDao.java
@@ -0,0 +1,21 @@
+package Project;
+
+import java.util.HashMap;
+
+import Project.BankBean;
+
+public class BankDao {
+	
+	BankBean beankBeanObj;
+	
+	HashMap<Long, BankBean> hm = new HashMap<Long, BankBean>();
+	
+	public void addCustomer(BankBean beankBeanObj) {			// METHOD TO ADD A CUSTOMER 
+		this.beankBeanObj = beankBeanObj;						// BY SAVING THE BANK BEAN OBJECT
+		hm.put(beankBeanObj.getAccNo(), beankBeanObj);			// IN HASH MAP
+	}
+	
+	public HashMap<Long, BankBean> hm(){						// METHOD TO RETURN HASH MAP OBJECT
+		return hm;
+	}
+}
